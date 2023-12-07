c53ad89ff1fd512d63fcd93a6373989196b8e2d6
jesus ramiro <jesus.ani@gmail.com>
2023-12-07 10:48:00 +0100

    build hecho

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/entrenadorpersonal1/',
+  base: '/entrenadorpersonal2/',
   plugins: [react()],
   test: {
     globals: true,
